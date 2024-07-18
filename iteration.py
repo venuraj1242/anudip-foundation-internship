e6980ad5442a752b4380ef1f6172bbe86cc778d0
venuraj1242 <venuraj1242@gmail.com>
2024-07-18 11:21:31 +0530

    comment

--- iteration.py
+++ iteration.py
@@ -1,50 +1,50 @@
 
 # itereation
 
-# for x in [80,99,90,50,30,40,50,302,200]:
-#     print(x)
+for x in [80,99,90,50,30,40,50,302,200]:
+    print(x)
 
 # find sum and avg
 
-# sum=0
-# l=[80,90,30,20,70,80,39,87]
-# for x in l:
-#     sum+=x
-# avg=sum/len(l)
-# print("sum=",sum,"avg=",avg)
+sum=0
+l=[80,90,30,20,70,80,39,87]
+for x in l:
+    sum+=x
+avg=sum/len(l)
+print("sum=",sum,"avg=",avg)
 
 # find out the number divisible by 10 and 7 both b/w 10 to 500
 
-# for x in range(10, 500):
-#     if (x % 10 == 0) and (x % 7 == 0):
-#         print(x)
+for x in range(10, 500):
+    if (x % 10 == 0) and (x % 7 == 0):
+        print(x)
 
 # count the number b/w 100 to 1000 which is even and divisible by 3
 
-# count=0
-# for x in range(100, 1000):
-#     if (x % 3 == 0) and (x % 2 == 0):
-#         count+=1
-# print("count=",count)
+count=0
+for x in range(100, 1000):
+    if (x % 3 == 0) and (x % 2 == 0):
+        count+=1
+print("count=",count)
 
 # use of end and \n
 
-# count=0
-# for x in range(100, 1000):
-#     if (x % 3 == 0) and (x % 2 == 0):
-#         print(x,end=',')
-#         count+=1
-# print("\n---------------------------------------------")
-# print("\ncount=",count)
+count=0
+for x in range(100, 1000):
+    if (x % 3 == 0) and (x % 2 == 0):
+        print(x,end=',')
+        count+=1
+print("\n---------------------------------------------")
+print("\ncount=",count)
 
 # while loop
 
-# x=100
-# count=0
-# while (x<1000):
-#     if (x % 3 == 0) and (x % 2 == 0):
-#         print(x,end=',')
-#         count+=1
-#     x+=1
-# print("\n---------------------------------------------")
-# print("\ncount=",count)
+x=100
+count=0
+while (x<1000):
+    if (x % 3 == 0) and (x % 2 == 0):
+        print(x,end=',')
+        count+=1
+    x+=1
+print("\n---------------------------------------------")
+print("\ncount=",count)
--- listfunc.py
+++ listfunc.py
@@ -1,36 +1,36 @@
 
 #append function
 list = [10, 20, 30, 40, 50, 60, 70, 80, 90]
-# x=int(input("enter the number"))
-# list.append(x)
-# print(list)
+x=int(input("enter the number"))
+list.append(x)
+print(list)
 
 # extend function 
 
-# list1=[110,120,130]
-# list.extend(list1)
-# print(list)
+list1=[110,120,130]
+list.extend(list1)
+print(list)
 
 # insert function 
 
-# print ( "insert 200 at 2 ")
-# list.insert(2,200)
-# print (list)
+print ( "insert 200 at 2 ")
+list.insert(2,200)
+print (list)
 
 #find a method to insert all the elements of another squence datatype at particular index in the list 
 #but all the elelments must be inserted one by one 
 
-# l=list[0:3]+[120,130,140]+list[3:]
-# print(l)
+l=list[0:3]+[120,130,140]+list[3:]
+print(l)
 
 #pop function 
-# print ( "remove at 2 ")
-# list.pop(2)
-# print (list)
+print ( "remove at 2 ")
+list.pop(2)
+print (list)
 
-# print ( "remove at last")
-# list.pop()
-# print (list)
+print ( "remove at last")
+list.pop()
+print (list)
 
 # remove()
 list.remove(90)
